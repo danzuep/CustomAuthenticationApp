71f95b8f7761d1f8692f9fbcd1437714a19afb0b
Daniel Collingwood <d.collingwood@truepartnercapital.com>
2023-08-23 17:10:41 +0800

    Added EnumHelper

---
+++ CustomAuthenticationApp/Extensions/EnumExtensions.cs
@@ -0,0 +1,41 @@
+﻿namespace CustomAuthenticationApp.Extensions
+{
+    internal static class EnumExtensions
+    {
+        public static string? ToUpper<T>(this T? obj) where T : Enum
+        {
+            string? value = null;
+            var all = EnumHelper<T>.KeyValuePairsUppercase;
+            if (obj is T key && !all.TryGetValue(key, out value))
+            {
+                value = key.ToString().ToUpperInvariant();
+                all.TryAdd(key, value);
+            }
+            return value;
+        }
+
+        public static string? ToLower<T>(this T? obj) where T : Enum
+        {
+            string? value = null;
+            var all = EnumHelper<T>.KeyValuePairsLowercase;
+            if (obj is T key && !all.TryGetValue(key, out value))
+            {
+                value = key.ToString().ToLowerInvariant();
+                all.TryAdd(key, value);
+            }
+            return value;
+        }
+
+        public static T[] AllValues<T>(this T _) where T : Enum =>
+            EnumHelper<T>.AllValues.Value;
+
+        public static string[] AllNames<T>(this T _) where T : Enum =>
+            EnumHelper<T>.AllNames.Value;
+
+        public static string[] AllNamesUppercase<T>(this T _) where T : Enum =>
+            EnumHelper<T>.AllNamesUppercase.Value;
+
+        public static string[] AllNamesLowercase<T>(this T _) where T : Enum =>
+            EnumHelper<T>.AllNamesLowercase.Value;
+    }
+}
---
+++ CustomAuthenticationApp/Extensions/EnumHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Concurrent;
+
+namespace CustomAuthenticationApp.Extensions
+{
+    internal static class EnumHelper<T> where T : Enum
+    {
+        public static Lazy<T[]> AllValues =>
+            new Lazy<T[]>(() => (T[])Enum.GetValues(typeof(T)));
+
+        public static Lazy<string[]> AllNames =>
+            new Lazy<string[]>(() => Enum.GetNames(typeof(T)));
+
+        public static Lazy<string[]> AllNamesUppercase =>
+            new Lazy<string[]>(() => AllNames.Value.Select(v => v.ToUpperInvariant()).ToArray());
+
+        public static Lazy<string[]> AllNamesLowercase =>
+            new Lazy<string[]>(() => AllNames.Value.Select(v => v.ToLowerInvariant()).ToArray());
+
+        public static readonly ConcurrentDictionary<T, string> KeyValuePairs = new();
+
+        public static readonly ConcurrentDictionary<T, string> KeyValuePairsUppercase = new();
+
+        public static readonly ConcurrentDictionary<T, string> KeyValuePairsLowercase = new();
+
+        static EnumHelper()
+        {
+            for (int i = 0; i < AllValues.Value.Length; i++)
+            {
+                KeyValuePairs[AllValues.Value[i]] = AllNames.Value[i];
+                KeyValuePairsUppercase[AllValues.Value[i]] = AllNamesUppercase.Value[i];
+                KeyValuePairsLowercase[AllValues.Value[i]] = AllNamesLowercase.Value[i];
+            }
+        }
+    }
+}
